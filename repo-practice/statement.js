720ce2c570250258bbbc02e544840a7711fd46af
jeanasong <58866533+jeanasong@users.noreply.github.com>
2020-11-19 16:01:13 -0500

    Add files via upload

---
+++ repo-practice/statement.js
@@ -0,0 +1,22 @@
+/* let x = 8
+if (x > 5) {
+    console.log("psoitive");
+} else {
+    console.log("negative");
+} */
+
+/* let user = 18
+if (user => 18 ) {
+    console.log("Accept");
+} else {
+    console.log("Denied");
+} */
+
+let num = 5
+if (num > 0 && num > 100) {
+    console.log("num if positove and greater than 100");
+} if (num > 0 && num < 100) {
+    console.log("num is positive and less tha 100");
+} if (num < 0) {
+    console.log("num is negative");
+}
