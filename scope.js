ec3d4fda30af81ed97d9280f4074d8f3f38a9f40
jeanasong <58866533+jeanasong@users.noreply.github.com>
2020-12-07 22:46:24 -0500

    class work two

---
+++ scope.js
@@ -0,0 +1,7 @@
+var x = 1;
+console.log("before loop " + x);
+function line5() {
+    var x = 2;
+    console.log("inside if " + x);
+}
+console.log("outside if " +x);
---
+++ statement.js
@@ -0,0 +1,26 @@
+/* let x = 8
+if (x > 5) {
+    console.log("psoitive");
+} else {
+    console.log("negative");
+} 
+
+  let user = 18
+if (user => 18 ) {
+    console.log("Accept");
+} else {
+    console.log("Denied");
+} 
+
+let num = 5
+if (num > 0 && num > 100) {
+    console.log("num if positove and greater than 100");
+} if (num > 0 && num < 100) {
+    console.log("num is positive and less tha 100");
+} if (num < 0) {
+    console.log("num is negative");
+} */
+
+const x = 2 + 2;
+debugger;
+console.log(x);
