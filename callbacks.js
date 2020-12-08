3f961b4d97ad0caa51aebace5b5db669a9bf0acd
jeanasong <58866533+jeanasong@users.noreply.github.com>
2020-12-07 22:43:49 -0500

    classwork

---
+++ callbacks.js
@@ -0,0 +1,38 @@
+//arrow function
+
+/* const bar = (param1) => {
+    console.log("callback");
+    param1();
+}
+
+bar( ()=> {
+    console.log("function 1");
+});
+
+bar( ()=> {
+    console.log("function 2");
+}); */
+
+// Write a function that adds three number in ES5
+
+/* function theSum(x,y,z){
+    return x+y+z
+};
+    sum=theSum (3,4,5);
+console.log(sum)
+
+// Now try converting the code to arrow functions
+theSum = (x,y,z) => x+y+z;
+console.log(theSum(3,4,5)); */
+
+// Write a function that adds three number and divide the result by 3 in ES5
+
+/* function theSum(x,y,z) {
+    return x+y+z
+};
+    sum=theSum (3,4,5);
+  console.log(sum/3);
+
+// Now try converting the code to arrow functions
+  theSum = (x,y,z) => x+y+z;
+  console.log(theSum(3,4,5)/3); */
---
+++ classwork.js
@@ -0,0 +1,31 @@
+
+/* let student = 90;
+if ('student => 90 || student => 80') {
+    console.log("A");
+} else {
+    console.log("B");
+}
+
+let studnet = 70;
+if ('student => 70 || student => 55') {
+    console.log("C");
+} else {
+    console.log("D");
+}
+
+let time = "5AM-11:59";
+if ('time >5AM && time < 11:59pm') {
+    console.log("Good Morning");
+} else {
+    console.log("Good Afternoon")
+} */
+
+// larger of two intergers //
+
+let interger = '5'
+if (interger >= 6) {
+    console.log("print the larger interger");
+} else {
+    console.log("print the smaller interger");
+}
+
